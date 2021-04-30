f2f19a71fb3e158419f65b844b73e4cc912d9325
chagassamuel <samuka_ptc@hotmail.com>
2021-04-30 11:32:53 -0300

    Abordando CORS e implementação de usuários em BD

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -19,17 +20,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("angular").secret("@ngul@r0").scopes("read", "write")
-				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(20)
-				.refreshTokenValiditySeconds(60 * 60 * 24); // 1 hora
+		clients.inMemory().withClient("angular").secret("$2a$10$G1j5Rf8aEEiGc/AET9BA..xRR.qCpOUzBZoJd8ygbGy6tb3jsMT9G")
+				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(20).refreshTokenValiditySeconds(60 * 60 * 24); // 1 dia
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter()).reuseRefreshTokens(false)
-				.authenticationManager(authenticationManager);
+				.userDetailsService(this.userDetailsService).authenticationManager(authenticationManager);
 	}
 
 	@Bean
